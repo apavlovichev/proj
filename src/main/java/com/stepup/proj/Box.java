34ef9d46ecbc5e184ce055bcf8e03aaeec6fa445
apavlovichev <apavlovichev@nota.tech>
2025-04-26 10:54:09 +0300

    Изменена основная логика и добавлен класс "Коробка"

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,17 @@
+package com.stepup.proj;
+
+public class Box {
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private int size;
+}
